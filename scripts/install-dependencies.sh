61219813b6569792ec4e65305cf26e7e2fab573d
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-08-15 19:50:09 -0400

    enhancement(install script): exit failure signal

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPTS_DIR" || exit
-cd "$(npm root)" || exit
-cd .. || exit
+cd "$SCRIPTS_DIR" || exit 1
+cd "$(npm root)" || exit 1
+cd .. || exit 1
 PROJECT_ROOT=$(pwd) # guaranteed to be in this project's root
 
 for arg in "$@"; do

cc98099bae77afec99e9b5bfeda1de3453c68202
アレクサンダー.eth <gpg@pavlovcik.com>
2022-05-20 17:02:17 +0800

    git submodule updated

--- scripts/update-submodules.sh
+++ scripts/update-submodules.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 git pull --recurse-submodules
 git submodule update --remote
+cd cx-opensea-cashback || exit
+git checkout develop
+cd ..
 git add .
 git commit -m "git submodule updated"
 git push origin
-cd cx-opensea-cashback || exit
-git checkout head
